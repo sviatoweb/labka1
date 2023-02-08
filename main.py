cf3af21f19ad9240d83f48a45799f80d2d255108
Sviatoslav <sviatkostehnii@gmail.com>
2023-02-08 15:50:49 +0200

    delete board variable

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-board = [
- "**** ****",
- "***1 ****",
- "**  3****",
- "* 4 1****",
- "     9 5 ",
- " 6  83  *",
- "3   1  **",
- "  8  2***",
- "  2  ****"
-]
-
 def check_column(board):
+    """
+    Checks whether columns are correct
+    """
     for i in range(9):
         numbers = '123456789'
         for line in board:
