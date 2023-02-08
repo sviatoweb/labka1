cba643d4ab2fd03f9dfbb63f2787a5273af0637c
skutarnataliia <skutar.pn@ucu.edu.ua>
2023-02-08 13:07:29 +0200

    add check_column

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ board = [
  "  8  2***",
  "  2  ****"
 ]
+
+def check_column(board):
+    for i in range(9):
+        numbers = '123456789'
+        for line in board:
+            if line[i] != '*' and line[i] != ' ':
+                index = numbers.find(line[i])
+                if index == -1:
+                    return False
+                numbers = numbers[:index] + numbers[index + 1:]
+    return True
+
+print(check_column(board))
